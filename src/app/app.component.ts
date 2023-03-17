617f40ceb35aca517585f7ddffa42842e1d68946
MarioQuirosL <marioql.dev@gmail.com>
2023-03-17 17:12:16 -0600

    Binding, addBootstrap, Sum, Subtract

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calculadora-Angular';
+  title = 'Calculadora Angular';
+
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+
+  sum() {
+    this.result = this.num1 + this.num2;
+  }
+  subtraction() {
+    this.result = this.num1 - this.num2;
+  }
+
 }
